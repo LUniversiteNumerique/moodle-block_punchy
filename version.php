aefad324b977d7dd09a65167ec51eb9cde43b5db
CarolineMVB <92635955+CarolineMVB@users.noreply.github.com>
2022-02-08 16:53:55 +0100

    create version.php in root folder

---
+++ version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2022020200;
+$plugin->requires = 2016052300;
+$plugin->release = '1.0.0';
+$plugin->component = 'punchy';
+$plugin->maturity = MATURITY_STABLE;
