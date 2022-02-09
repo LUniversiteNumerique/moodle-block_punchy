9550534d149fa01f5a7afceb9a1d9a5b3753c1d6
Hipjea <pierre.duverneix@gmail.com>
2022-02-09 13:03:23 +0100

    edit_form comments

--- edit_form.php
+++ edit_form.php
@@ -9,12 +9,12 @@ class block_punchy_edit_form extends block_edit_form
         // Titre de l'en-tête de section selon le fichier de langue.
         $mform->addElement('header', 'config_header', get_string('blocksettings', 'block'));
 
-        // MODIFIER LE TITRE DU BLOC
+        // Block title.
         $mform->addElement('text', 'config_title', get_string('blocktitle', 'block_punchy'));
         $mform->setDefault('config_title', "Partageons l'Université Numérique et des Cursus HYbrides");
         $mform->setType('config_title', PARAM_TEXT);
 
-        // MODIFIER LE CONTENU DU BLOC
+        // Block text.
         $mform->addElement('editor', 'config_text', get_string('blockstring', 'block_punchy'), null, get_string('defaulttext', 'block_punchy'));
         $mform->setType('config_text', PARAM_RAW);
     }
