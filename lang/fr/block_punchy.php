3bb051b3db9fc073954f5c1451bfde6fa28adb8a
CarolineMVB <92635955+CarolineMVB@users.noreply.github.com>
2022-02-09 15:28:30 +0100

    Final Version 0.1

--- lang/fr/block_punchy.php
+++ lang/fr/block_punchy.php
@@ -1,12 +1,30 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * PUNCHY block
+ *
+ * @package    block_punchy
+ * @copyright  2022 L'Université Numérique
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 $string['pluginname'] = 'PUNCHY';
 $string['title'] = "Partageons l'Université Numérique et des Cursus HYbrides";
 $string['blocktitle'] = 'Modifier le titre :';
 $string['blockstring'] = 'Modifier le contenu :';
 $string['defaulttitle'] = "Partageons l'Université Numérique et des Cursus HYbrides";
-$string['defaulttext'] = "<p>Un projet de L'Université Numérique alliée à un consortium de partenaires :<p>
-                            <ul>
-                                <li>Pour répondre à une forte demande de supports de formations hybrides facilement réutilisables</li>
-                                <li>Pour engager une démarche générale, systémique et collective visant la création et le partage de ressources pédagogiques numériques à large potentiel de réutilisation, révision et adaptation à des contextes divers</li>
-                            </ul>";
+$string['defaulttext'] = "<p>Un projet de L'Université Numérique alliée à un consortium de partenaires :<p><ul><li>Pour répondre à une forte demande de supports de formations hybrides facilement réutilisables</li><li>Pour engager une démarche générale, systémique et collective visant la création et le partage de ressources pédagogiques numériques à large potentiel de réutilisation, révision et adaptation à des contextes divers</li></ul>";
