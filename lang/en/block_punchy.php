a061e63abd236fe0227d726e5f77fef68f45079e
Hipjea <pierre.duverneix@gmail.com>
2022-02-14 11:46:28 +0100

    add imagealt, block_punchy to image alt

--- lang/en/block_punchy.php
+++ lang/en/block_punchy.php
@@ -23,13 +23,14 @@
  */
 
 $string['pluginname'] = 'PUNCHY';
-$string['title'] = 'Partageons l\'Université Numérique et des Cursus HYbrides';
+$string['title'] = 'Partageons L\'Université Numérique et des Cursus HYbrides';
 $string['blocktitle'] = 'Change the title :';
 $string['blockstring'] = 'Change the content :';
-$string['defaulttitle'] = 'Partageons l\'Université Numérique et des Cursus HYbrides';
+$string['defaulttitle'] = 'Partageons L\'Université Numérique et des Cursus HYbrides';
 $string['defaulttext'] = '<p>A project of the Digital University allied to a consortium of partners:<p>
 <ul>
     <li>To meet a strong demand for easily reusable hybrid training materials</li>
     <li>To initiate a general, systemic and collective approach aimed at the creation and sharing of digital educational 
     resources with a large potential for reuse, revision and adaptation to various contexts</li>
 </ul>';
+$string['imagealt'] = 'Logos of France Relance, Government of France, L\'Université Numérique, Fondation UNIT, European Union';
