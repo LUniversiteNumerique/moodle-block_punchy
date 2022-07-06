88099b00ea79868957258bf0de13ebe5ecf4361c
Hipjea <pierre.duverneix@gmail.com>
2022-07-06 16:20:01 +0200

    licence

--- classes/output/configure_page.php
+++ classes/output/configure_page.php
@@ -14,6 +14,13 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * PUNCHY block
+ *
+ * @package    block_punchy
+ * @copyright  2022 L'Université Numérique
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 namespace block_punchy\output;
 
